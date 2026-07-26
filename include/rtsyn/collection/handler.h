e8781b3205a124522ddbfde824d737e33e88ec9d
seregio <sergiohg818@gmail.com>
2026-07-26 12:58:11 +0200

    collection/handler: Add collection handler header

---
+++ include/rtsyn/collection/handler.h
@@ -0,0 +1,19 @@
+/**
+ * @file rtsyn/handler.h
+ * @author Sergio Hidalgo (sergiohg.dev@gmail.com)
+ * @brief Header file for the RTSyn Handler.
+ *
+ * The RTSyn Runtime Handler abstracts aggregations of objets.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * @copyright Copyright (c) Sergio Hidalgo 2026
+ */
+#ifndef RTSYN_COLLECTION_HANDLER_H
+#define RTSYN_COLLECTION_HANDLER_H
+
+/**
+ * @brief Opaque handler.
+ */
+typedef struct rtsyn_collection_handler_s rtsyn_collection_handler_t;
+
+#endif /* RTSYN_COLLECTION_HANDLER_H */
