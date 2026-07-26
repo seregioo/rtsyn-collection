a8c9a975febe83f6c8601bc64736bdb41cc0d8c0
seregio <sergiohg818@gmail.com>
2026-07-26 12:59:10 +0200

    collection/instance: Add collection instance header

---
+++ include/rtsyn/collection/instance.h
@@ -0,0 +1,17 @@
+/**
+ * @file rtsyn/collection/instance.h
+ * @author Sergio Hidalgo (sergiohg.dev@gmail.com)
+ * @brief Header file for the RTSyn Collection Instance.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * @copyright Copyright (c) Sergio Hidalgo 2026
+ */
+#ifndef RTSYN_COLLECTION_INSTANCE_H
+#define RTSYN_COLLECTION_INSTANCE_H
+
+/**
+ * @brief Opaque array.
+ */
+typedef struct rtsyn_collection_instance_s rtsyn_collection_instance_t;
+
+#endif /* RTSYN_COLLECTION_INSTANCE_ARRAY_H */
